c0a783d7c6085fbe42acb278c134f7c859f6ac61
Lauris <lauriselvijsm@gmail.com>
2023-01-09 17:01:18 +0200

    updated style

--- src/services/Advice.service.ts
+++ src/services/Advice.service.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { ADVICE_FETCH_URL } from "../constants/AdviceFetch.const";
+import { Advice } from "../types/Advice";
 
 export const fetchAdvice = async () => {
   const data = await axios.get<Advice>(ADVICE_FETCH_URL);
